d52b63d262db83ccda36e286bbb218b8c4d7c674
Michael <michael@gmail.com>
2024-11-18 18:07:29 +0100

    exercise 2 done

--- labs/02-Java-oop_concepts-inheritance/src/Dog.java
+++ labs/02-Java-oop_concepts-inheritance/src/Dog.java
@@ -1,7 +1,24 @@
 public class Dog extends Animal {
     //TODO have a default constructor for Dog, with 4 legs and 2 eyes!
 
+    public Dog() {
+        super(4, 2);
+    }
+
     //TODO override the breathe() method!
 
+    @Override
+    public final void breathe() {
+        super.breathe();
+        System.out.println("Dogs love to breathe with their mouths open.");
+    }
+
+
     //TODO override the walk() method!
+
+    @Override
+    public void walk() {
+        super.walk();
+        System.out.println("Dogs love to run.");
+    }
 }
--- labs/02-Java-oop_concepts-inheritance/src/GermanShepard.java
+++ labs/02-Java-oop_concepts-inheritance/src/GermanShepard.java
@@ -1,4 +1,10 @@
 public class GermanShepard extends Dog {
 
     //TODO override the walk() method
+
+    @Override
+    public void walk() {
+        super.walk();
+        System.out.println("German Shepard`s show their beautiful fur while running.");
+    }
 }
