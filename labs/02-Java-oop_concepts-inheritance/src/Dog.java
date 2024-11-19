965b21bd7d6c4a1672829e39f67fde2aef5a2aec
Hasan <5iveatc@gmail.com>
2024-11-19 07:47:44 +0100

    animal Reign is done

--- labs/02-Java-oop_concepts-inheritance/src/Dog.java
+++ labs/02-Java-oop_concepts-inheritance/src/Dog.java
@@ -1,7 +1,18 @@
 public class Dog extends Animal {
     //TODO have a default constructor for Dog, with 4 legs and 2 eyes!
+    public Dog(){
+        super(4,2);
+    }
 
     //TODO override the breathe() method!
 
+    public final void breathe() {
+        System.out.println("TheDogs love to breathe with their mouths open.");
+    }
+
     //TODO override the walk() method!
+
+    public void walk() {
+        System.out.println("Dogs love to run.");
+    }
 }
